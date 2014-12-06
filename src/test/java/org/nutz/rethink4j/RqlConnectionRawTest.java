da2394826698586dc57dd2bc9e53f809d19ba0f6
wendal <wendal1985@gmail.com>
2014-12-06 20:26:36 +0800

    add: fiter by javascript, done

--- src/test/java/org/nutz/rethink4j/RqlConnectionRawTest.java
+++ src/test/java/org/nutz/rethink4j/RqlConnectionRawTest.java
@@ -2,6 +2,7 @@ package org.nutz.rethink4j;
 
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -32,6 +33,9 @@ public class RqlConnectionRawTest {
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Test
 	public void test_raw_db_list() {
+		// 带上profile
+		Map<String, Term> optargs = new HashMap<String, Term>();
+		//optargs.put("profile", Term.mkDatum(true));
 		
 		Term dbList = Term.mk("db_list"); // java. r.dbList().run(conn);
 		NutMap map = conn.startQuery(0, dbList, null);
@@ -98,7 +102,7 @@ public class RqlConnectionRawTest {
 		
 		// 试试一些无参数的方法
 		Term uuid = Term.mk("uuid");
-		map = conn.startQuery(0, uuid, null);
+		map = conn.startQuery(0, uuid, optargs);
 		checkSuccessAtom(map);
 		r = map.getAs("r", List.class);
 		assertEquals(1, r.size());
@@ -107,38 +111,50 @@ public class RqlConnectionRawTest {
 		System.out.println(_id);
 		
 		// 把表建回来
-		map = conn.startQuery(0, tableCreate, null);
+		map = conn.startQuery(0, tableCreate, optargs);
 		checkSuccessAtom(map);
 		
 		// 插入一条数据
-		NutMap user_wendal = new NutMap();
-		user_wendal.put("id", _id.toString());
-		user_wendal.put("age", 29);
-		user_wendal.put("city", "gz");
-		Term insert = Term.mk("insert", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), Term.mkDatum(user_wendal));
-		map = conn.startQuery(0, insert, null);
+		NutMap user_rp = new NutMap();
+		user_rp.put("id", _id.toString());
+		user_rp.put("name", "mary");
+		user_rp.put("age", 29);
+		user_rp.put("city", "gz");
+		Term insert = Term.mk("insert", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), Term.mkDatum(user_rp));
+		map = conn.startQuery(0, insert, optargs);
 		checkSuccessAtom(map);
 		
 		// 再插入一条
-		user_wendal = new NutMap();
-		user_wendal.put("id", UUID.randomUUID().toString());
-		user_wendal.put("age", 90);
-		user_wendal.put("city", "richmond");
-		insert = Term.mk("insert", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), Term.mkDatum(user_wendal));
-		map = conn.startQuery(0, insert, null);
+		user_rp = new NutMap();
+		user_rp.put("id", UUID.randomUUID().toString());
+		user_rp.put("name", "wendal");
+		user_rp.put("age", 90);
+		user_rp.put("city", "richmond");
+		insert = Term.mk("insert", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), Term.mkDatum(user_rp));
+		map = conn.startQuery(0, insert, optargs);
+		checkSuccessAtom(map);
+		
+		// 插入第3条
+		user_rp = new NutMap();
+		user_rp.put("id", UUID.randomUUID().toString());
+		user_rp.put("name", "zozoh");
+		user_rp.put("age", 35);
+		user_rp.put("city", "beijing");
+		insert = Term.mk("insert", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), Term.mkDatum(user_rp));
+		map = conn.startQuery(0, insert, optargs);
 		checkSuccessAtom(map);
 		
 		// 看看总数
 		
 		Term user_count = Term.mk("count", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)));
-		map = conn.startQuery(0, user_count, null);
+		map = conn.startQuery(0, user_count, optargs);
 		checkSuccessAtom(map);
 		r = map.getAs("r", List.class);
-		assertEquals(2, r.get(0));
+		assertEquals(3, r.get(0));
 		
 		// 取出第一个user (已知id)
 		Term user_get = Term.mk("get", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), Term.mkDatum(_id.toString()));
-		map = conn.startQuery(0, user_get, null);
+		map = conn.startQuery(0, user_get, optargs);
 		checkSuccessAtom(map);
 		r = map.getAs("r", List.class);
 		Map<String, Object> user = (Map<String, Object>) r.get(0);
@@ -147,7 +163,7 @@ public class RqlConnectionRawTest {
 		
 		// 取出age=90的user, 直接匹配方法, 传Map
 		Term user_age_90 = Term.mk("filter", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), Term.mkDatum(new NutMap().setv("age", 90)));
-		map = conn.startQuery(0, user_age_90, null);
+		map = conn.startQuery(0, user_age_90, optargs);
 		checkSuccessSequence(map);
 		r = map.getAs("r", List.class);
 		user = (Map<String, Object>) r.get(0);
@@ -157,7 +173,7 @@ public class RqlConnectionRawTest {
 		Term eg = Term.mk("eq", Term.mk("bracket", Term.mk("implicit_var"), Term.mkDatum("age")), Term.mkDatum(90));
 		Term func = Term.mk("func", Term.mk("make_array", Term.mkDatum(2)), eg);
 		user_age_90 = Term.mk("filter", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), func);
-		map = conn.startQuery(0, user_age_90, null);
+		map = conn.startQuery(0, user_age_90, optargs);
 		checkSuccessSequence(map);
 		r = map.getAs("r", List.class);
 		user = (Map<String, Object>) r.get(0);
@@ -171,7 +187,7 @@ public class RqlConnectionRawTest {
 		Term any_city = Term.mk("any", eg_city_gz, eg_city_richmond);
 		func = Term.mk("func", Term.mk("make_array", Term.mkDatum(4)), any_city);
 		Term user_any_city = Term.mk("filter", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), func);
-		map = conn.startQuery(0, user_any_city, null);
+		map = conn.startQuery(0, user_any_city, optargs);
 		checkSuccessSequence(map);
 		r = map.getAs("r", List.class);
 		assertEquals(2, r.size());
@@ -184,6 +200,32 @@ public class RqlConnectionRawTest {
 		assertEquals(2, r.size());
 		assertEquals(90, ((Map)r.get(0)).get("age"));
 		assertEquals(29, ((Map)r.get(1)).get("age"));
+		
+		// 将对city的查询改成js形式
+		
+		Term city_js = Term.mk("javascript", Term.mkDatum("(function (r) { return r.city == 'gz' || r.city == 'richmond';})"));
+		user_any_city = Term.mk("filter", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), city_js);
+		map = conn.startQuery(0, user_any_city, optargs);
+		checkSuccessSequence(map);
+		r = map.getAs("r", List.class);
+		assertEquals(2, r.size());
+		
+		// 单单返回city为gz的记录
+		city_js = Term.mk("javascript", Term.mkDatum("(function (r) { return r.city == 'gz';})"));
+		user_any_city = Term.mk("filter", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), city_js);
+		map = conn.startQuery(0, user_any_city, optargs);
+		checkSuccessSequence(map);
+		r = map.getAs("r", List.class);
+		assertEquals(1, r.size());
+		
+		// 年龄小于40的记录
+		Term age_js = Term.mk("javascript", Term.mkDatum("(function (r) { return r.age < 40;})"));
+		Term user_age = Term.mk("filter", Term.mk("table", Term.mk("db", Term.mkDatum(dbName)), Term.mkDatum(tableName)), age_js);
+		map = conn.startQuery(0, user_age, optargs);
+		checkSuccessSequence(map);
+		r = map.getAs("r", List.class);
+		assertEquals(2, r.size());
+		System.out.println(map.get("p"));
 	}
 
 	public void checkSuccessAtom(NutMap map) {
